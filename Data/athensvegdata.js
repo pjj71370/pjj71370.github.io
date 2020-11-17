057dfaceca573b198714b6fdf472541abb50b141
Paris Johnson <51677972+pjj71370@users.noreply.github.com>
2020-11-16 23:17:38 -0500

    Update athensvegdata.js

--- Data/athensvegdata.js
+++ Data/athensvegdata.js
@@ -136,4 +136,143 @@
         }
       ],
       "id": "1"
+  },
+
+    {
+        "restaurant_name": "The Grit",
+        "brief_description": "Vegetarian Fare Made Delicious",
+        "address_1": "123 Broad St.",
+        "address_2": "",
+        "city_town": "Athens",
+        "state_province": "GA",
+        "postal_code": "30601",
+        "country": "US",
+        "phone": "706-552-0051",
+        "website_url": "http://thegrit.com",
+      "menus": [
+        {
+          "menu_name": "The Grit Menu",
+          "menu_description": "We believe you don’t need to sacrifice your health to get a quick, tasty meal. That’s why we start with our unique soft and roll-able pita bread, then fill it with your choice of lean, grilled meats, fresh vegetables, flavorful cheeses, and savory sauces.",
+          "menu_groups": [
+            {
+              "group_name": "Full",
+              "menu_items": [
+                {
+                  "menu_item_name": "Dagwood",
+                  "menu_item_description": "Turkey, black forest ham, prime rib, shredded lettuce, tomatoes, onions, cheddar cheese, light mayo, horseradish dijon"
+                },
+                {
+                  "menu_item_name": "Gyro",
+                  "menu_item_description": "Mediterranean seasoned beef, romaine, tomatoes, cucumbers, onions, black olives, feta cheese, tzatziki"
+                },
+                {
+                  "menu_item_name": "Philly Steak",
+                  "menu_item_description": "Thinly sliced steak, grilled onions, grilled green peppers, grilled mushrooms, melted provolone cheese, ancho chipotle sauce"
+                
+                },
+                {
+                  "menu_item_name": "Prime Rib",
+                  "menu_item_description": "Prime rib, shredded lettuce, tomatoes, onions, green peppers, cheddar cheese, horseradish dijon"
+                 
+                },
+                {
+                  "menu_item_name": "Tuna",
+                  "menu_item_description": "Tuna, shredded lettuce, tomatoes, pickles, cheddar cheese, light mayo.."
+                 
+                },
+                {
+                  "menu_item_name": "Turkey",
+                  "menu_item_description": "Deli style turkey, shredded lettuce, tomatoes, cucumbers, provolone cheese, light mayo"
+                 
+                },
+                {
+                  "menu_item_name": "Club",
+                  "menu_item_description": "Black forest ham, turkey, bacon, shredded lettuce, tomatoes, provolone cheese, light honey dijon"
+                  
+                },
+                {
+                  "menu_item_name": "Chicken Souvlaki",
+                  "menu_item_description": "Mediterranean seasoned chicken, spinach, tomatoes, cucumbers, onions, feta cheese, tzatziki"
+                
+                },
+                {
+                  "menu_item_name": "Chicken Crave®",
+                  "menu_item_description": "Chicken breast, black forest ham, romaine, onions, provolone cheese, light honey dijon"
+                  
+                },
+                {
+                  "menu_item_name": "Chicken Caesar",
+                  "menu_item_description": "Chicken breast, bacon, romaine, parmesan cheese, caesar dressing"
+                
+                },
+                {
+                  "menu_item_name": "Chicken Breast",
+                  "menu_item_description": "Chicken breast, shredded lettuce, tomatoes, cheddar cheese, ranch dressing"
+                
+                },
+                {
+                  "menu_item_name": "Buffalo Chicken",
+                  "menu_item_description": "Chicken breast grilled in buffalo sauce, romaine, tomatoes, onions, green peppers, provolone cheese, ranch"
+                  
+                },
+                {
+                  "menu_item_name": "B.L.T",
+                  "menu_item_description": "Bacon, shredded lettuce, tomatoes, light mayo"
+                
+                },
+                {
+                  "menu_item_name": "Black Forest Ham",
+                  "menu_item_description": "Black forest ham, shredded lettuce, tomatoes, provolone cheese, yellow mustard, light mayo"
+                
+            }
+            ]
+            },
+            {
+              "group_name": "Vegan",
+             "menu_items": [
+                {
+                  "menu_item_name": "Vegan Dagwood",
+                  "menu_item_description": "Vegan Turkey, black forest ham, prime rib, shredded lettuce, tomatoes, onions, cheddar cheese, light mayo, horseradish dijon"
+                
+                },
+                {
+                  "menu_item_name": "Vegan Gyro",
+                  "menu_item_description": "Vegan Mediterranean seasoned beef, romaine, tomatoes, cucumbers, onions, black olives, feta cheese, tzatziki"
+                 
+                },
+                {
+                  "menu_item_name": "Vegan Philly Steak",
+                  "menu_item_description": "Vegan Thinly sliced steak, grilled onions, grilled green peppers, grilled mushrooms, melted provolone cheese, ancho chipotle sauce"
+             
+                }
+                
+            ]
+            },
+            {
+              "group_name": "Vegetarian",
+                   "menu_items": [
+                {
+                  "menu_item_name": "Vegetarian Turkey",
+                  "menu_item_description": "Vegetarian Deli style turkey, shredded lettuce, tomatoes, cucumbers, provolone cheese, light mayo"
+                 
+                },
+                {
+                  "menu_item_name": "Vegetarian Club",
+                  "menu_item_description": "Vegetarian Black forest ham, turkey, bacon, shredded lettuce, tomatoes, provolone cheese, light honey dijon"
+                
+                },
+                {
+                  "menu_item_name": "Vegetarian Chicken Souvlaki",
+                  "menu_item_description": "Vegetarian Mediterranean seasoned chicken, spinach, tomatoes, cucumbers, onions, feta cheese, tzatziki"
+                },
+                {
+                  "menu_item_name": "Vegetarian Chicken Crave®",
+                  "menu_item_description": "Chicken breast, black forest ham, romaine, onions, provolone cheese, light honey dijon"
+                }
+            ]
+            }
+              ]
+        }
+      ],
+      "id": "2"
   }]
