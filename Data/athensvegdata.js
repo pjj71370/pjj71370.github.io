0fea3b3f9f619aafbfc1cf6fb81b55e2bf6dcde2
Paris Johnson <51677972+pjj71370@users.noreply.github.com>
2020-11-18 10:58:43 -0500

    Update athensvegdata.js

--- Data/athensvegdata.js
+++ Data/athensvegdata.js
@@ -275,4 +275,280 @@
         }
       ],
       "id": "2"
+  },
+{
+        "restaurant_name": "Tzazikis",
+        "brief_description": "Greek and Mediterreanean",
+        "address_1": "125 North St.",
+        "address_2": "",
+        "city_town": "Athens",
+        "state_province": "GA",
+        "postal_code": "30601",
+        "country": "US",
+        "phone": "706-552-0051",
+        "website_url": "http://tzazikis.com",
+      "menus": [
+        {
+          "menu_name": "Tzazikis Menu",
+          "menu_description": "Tzazikis is the best place for you Mediterreanean needs.",
+          "menu_groups": [
+            {
+              "group_name": "Full",
+              "menu_items": [
+                {
+                  "menu_item_name": "Greek Gyro",
+                  "menu_item_description": "Gryo filled with the best"
+                },
+                {
+                  "menu_item_name": "Pita Bread",
+                  "menu_item_description": "Pita bread that's very light and fluffy"
+                },
+                {
+                  "menu_item_name": "Philly Steak",
+                  "menu_item_description": "Steak and gravy"
+                
+                },
+                {
+                  "menu_item_name": "Prime Rib",
+                  "menu_item_description": "Prime rib, shredded lettuce, tomatoes, onions, green peppers, cheddar cheese, horseradish dijon"
+                 
+                },
+                {
+                  "menu_item_name": "Tuna",
+                  "menu_item_description": "Tuna, shredded lettuce, tomatoes, pickles, cheddar cheese, light mayo.."
+                 
+                },
+                {
+                  "menu_item_name": "Turkey",
+                  "menu_item_description": "Deli style turkey, shredded lettuce, tomatoes, cucumbers, provolone cheese, light mayo"
+                 
+                },
+                {
+                  "menu_item_name": "Club",
+                  "menu_item_description": "Black forest ham, turkey, bacon, shredded lettuce, tomatoes, provolone cheese, light honey dijon"
+                  
+                },
+                {
+                  "menu_item_name": "Chicken Souvlaki",
+                  "menu_item_description": "Mediterranean seasoned chicken, spinach, tomatoes, cucumbers, onions, feta cheese, tzatziki"
+                
+                },
+                {
+                  "menu_item_name": "Chicken Crave®",
+                  "menu_item_description": "Chicken breast, black forest ham, romaine, onions, provolone cheese, light honey dijon"
+                  
+                },
+                {
+                  "menu_item_name": "Chicken Caesar",
+                  "menu_item_description": "Chicken breast, bacon, romaine, parmesan cheese, caesar dressing"
+                
+                },
+                {
+                  "menu_item_name": "Chicken Breast",
+                  "menu_item_description": "Chicken breast, shredded lettuce, tomatoes, cheddar cheese, ranch dressing"
+                
+                },
+                {
+                  "menu_item_name": "Buffalo Chicken",
+                  "menu_item_description": "Chicken breast grilled in buffalo sauce, romaine, tomatoes, onions, green peppers, provolone cheese, ranch"
+                  
+                },
+                {
+                  "menu_item_name": "B.L.T",
+                  "menu_item_description": "Bacon, shredded lettuce, tomatoes, light mayo"
+                
+                },
+                {
+                  "menu_item_name": "Black Forest Ham",
+                  "menu_item_description": "Black forest ham, shredded lettuce, tomatoes, provolone cheese, yellow mustard, light mayo"
+                
+            }
+            ]
+            },
+            {
+              "group_name": "Vegan",
+             "menu_items": [
+                {
+                  "menu_item_name": "Vegan Dagwood",
+                  "menu_item_description": "Vegan Turkey, black forest ham, prime rib, shredded lettuce, tomatoes, onions, cheddar cheese, light mayo, horseradish dijon"
+                
+                },
+                {
+                  "menu_item_name": "Vegan Gyro",
+                  "menu_item_description": "Vegan Mediterranean seasoned beef, romaine, tomatoes, cucumbers, onions, black olives, feta cheese, tzatziki"
+                 
+                },
+                {
+                  "menu_item_name": "Vegan Philly Steak",
+                  "menu_item_description": "Vegan Thinly sliced steak, grilled onions, grilled green peppers, grilled mushrooms, melted provolone cheese, ancho chipotle sauce"
+             
+                }
+                
+            ]
+            },
+            {
+              "group_name": "Vegetarian",
+                   "menu_items": [
+                {
+                  "menu_item_name": "Vegetarian Turkey",
+                  "menu_item_description": "Vegetarian Deli style turkey, shredded lettuce, tomatoes, cucumbers, provolone cheese, light mayo"
+                 
+                },
+                {
+                  "menu_item_name": "Vegetarian Club",
+                  "menu_item_description": "Vegetarian Black forest ham, turkey, bacon, shredded lettuce, tomatoes, provolone cheese, light honey dijon"
+                
+                },
+                {
+                  "menu_item_name": "Vegetarian Chicken Souvlaki",
+                  "menu_item_description": "Vegetarian Mediterranean seasoned chicken, spinach, tomatoes, cucumbers, onions, feta cheese, tzatziki"
+                },
+                {
+                  "menu_item_name": "Vegetarian Chicken Crave®",
+                  "menu_item_description": "Chicken breast, black forest ham, romaine, onions, provolone cheese, light honey dijon"
+                }
+            ]
+            }
+              ]
+        }
+      ],
+      "id": "3"
+  },
+{
+        "restaurant_name": "Frutta Bowls",
+        "brief_description": "Smoothie Bowls made delicious",
+        "address_1": "143 Broad St.",
+        "address_2": "",
+        "city_town": "Athens",
+        "state_province": "GA",
+        "postal_code": "30601",
+        "country": "US",
+        "phone": "706-552-0051",
+        "website_url": "http://fruttabowls.com",
+      "menus": [
+        {
+          "menu_name": "Frutta Bowls Menu",
+          "menu_description": "Delicious Smoothie Bowls",
+          "menu_groups": [
+            {
+              "group_name": "Full",
+              "menu_items": [
+                {
+                  "menu_item_name": "Green Smoothie",
+                  "menu_item_description": "Cucumbers, apples, bananas, kale"
+                },
+                {
+                  "menu_item_name": "Red Smoothie",
+                  "menu_item_description": "Beets, carrots, appples"
+                },
+                {
+                  "menu_item_name": "Orange Smoothie",
+                  "menu_item_description": "Oranges, peaches, bananas"
+                
+                },
+                {
+                  "menu_item_name": "Yellow Smoothie",
+                  "menu_item_description": "Bananas, lemon, squash"
+                 
+                },
+                {
+                  "menu_item_name": "Tuna",
+                  "menu_item_description": "Tuna, shredded lettuce, tomatoes, pickles, cheddar cheese, light mayo.."
+                 
+                },
+                {
+                  "menu_item_name": "Turkey",
+                  "menu_item_description": "Deli style turkey, shredded lettuce, tomatoes, cucumbers, provolone cheese, light mayo"
+                 
+                },
+                {
+                  "menu_item_name": "Club",
+                  "menu_item_description": "Black forest ham, turkey, bacon, shredded lettuce, tomatoes, provolone cheese, light honey dijon"
+                  
+                },
+                {
+                  "menu_item_name": "Chicken Souvlaki",
+                  "menu_item_description": "Mediterranean seasoned chicken, spinach, tomatoes, cucumbers, onions, feta cheese, tzatziki"
+                
+                },
+                {
+                  "menu_item_name": "Chicken Crave®",
+                  "menu_item_description": "Chicken breast, black forest ham, romaine, onions, provolone cheese, light honey dijon"
+                  
+                },
+                {
+                  "menu_item_name": "Chicken Caesar",
+                  "menu_item_description": "Chicken breast, bacon, romaine, parmesan cheese, caesar dressing"
+                
+                },
+                {
+                  "menu_item_name": "Chicken Breast",
+                  "menu_item_description": "Chicken breast, shredded lettuce, tomatoes, cheddar cheese, ranch dressing"
+                
+                },
+                {
+                  "menu_item_name": "Buffalo Chicken",
+                  "menu_item_description": "Chicken breast grilled in buffalo sauce, romaine, tomatoes, onions, green peppers, provolone cheese, ranch"
+                  
+                },
+                {
+                  "menu_item_name": "B.L.T",
+                  "menu_item_description": "Bacon, shredded lettuce, tomatoes, light mayo"
+                
+                },
+                {
+                  "menu_item_name": "Black Forest Ham",
+                  "menu_item_description": "Black forest ham, shredded lettuce, tomatoes, provolone cheese, yellow mustard, light mayo"
+                
+            }
+            ]
+            },
+            {
+              "group_name": "Vegan",
+             "menu_items": [
+                {
+                  "menu_item_name": "Vegan Dagwood",
+                  "menu_item_description": "Vegan Turkey, black forest ham, prime rib, shredded lettuce, tomatoes, onions, cheddar cheese, light mayo, horseradish dijon"
+                
+                },
+                {
+                  "menu_item_name": "Vegan Gyro",
+                  "menu_item_description": "Vegan Mediterranean seasoned beef, romaine, tomatoes, cucumbers, onions, black olives, feta cheese, tzatziki"
+                 
+                },
+                {
+                  "menu_item_name": "Vegan Philly Steak",
+                  "menu_item_description": "Vegan Thinly sliced steak, grilled onions, grilled green peppers, grilled mushrooms, melted provolone cheese, ancho chipotle sauce"
+             
+                }
+                
+            ]
+            },
+            {
+              "group_name": "Vegetarian",
+                   "menu_items": [
+                {
+                  "menu_item_name": "Vegetarian Turkey",
+                  "menu_item_description": "Vegetarian Deli style turkey, shredded lettuce, tomatoes, cucumbers, provolone cheese, light mayo"
+                 
+                },
+                {
+                  "menu_item_name": "Vegetarian Club",
+                  "menu_item_description": "Vegetarian Black forest ham, turkey, bacon, shredded lettuce, tomatoes, provolone cheese, light honey dijon"
+                
+                },
+                {
+                  "menu_item_name": "Vegetarian Chicken Souvlaki",
+                  "menu_item_description": "Vegetarian Mediterranean seasoned chicken, spinach, tomatoes, cucumbers, onions, feta cheese, tzatziki"
+                },
+                {
+                  "menu_item_name": "Vegetarian Chicken Crave®",
+                  "menu_item_description": "Chicken breast, black forest ham, romaine, onions, provolone cheese, light honey dijon"
+                }
+            ]
+            }
+              ]
+        }
+      ],
+      "id": "4"
   }]
