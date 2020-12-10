15ee7b2889e6eb1037deac2dfe2d79e4ef9a121c
Paris Johnson <51677972+pjj71370@users.noreply.github.com>
2020-12-10 17:37:39 -0500

    Update and rename athvegdata.js to athensvegreviews.js

---
+++ Data/athensvegreviews.js
@@ -0,0 +1,29 @@
+[
+    {
+        "reviewer_name": "Jackson P.",
+        "rest_name": "The Grit",
+        "review_title": "Good Food, Great Service ",
+        "item_name": "Veggie Scramble",
+        "rating": "4 out of 5",
+        "review_body": "I loved my meal and the staff was super accomadating with substitutions. A super great menu selection too. Will definitely be getting the Veggie Scramble again.",
+    },
+    
+    {
+        "reviewer_name": "Jackson P.",
+        "rest_name": "The Grit",
+        "review_title": "Good Food, Great Service ",
+        "item_name": "Veggie Scramble",
+        "rating": "4 out of 5",
+        "review_body": "I loved my meal and the staff was super accomadating with substitutions. A super great menu selection too. Will definitely be getting the Veggie Scramble again.",
+    },
+    
+    {
+        "reviewer_name": "Jackson P.",
+        "rest_name": "The Grit",
+        "review_title": "Good Food, Great Service ",
+        "item_name": "Veggie Scramble",
+        "rating": "4 out of 5",
+        "review_body": "I loved my meal and the staff was super accomadating with substitutions. A super great menu selection too. Will definitely be getting the Veggie Scramble again.",
+    },
+
+
