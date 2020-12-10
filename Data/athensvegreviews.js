e35190b84a51180e2897360111dbdc829c64e094
Paris Johnson <51677972+pjj71370@users.noreply.github.com>
2020-12-10 18:41:57 -0500

    Update athensvegreviews.js

--- Data/athensvegreviews.js
+++ Data/athensvegreviews.js
@@ -24,6 +24,7 @@
         "item_name": "Veggie Scramble",
         "rating": "4 out of 5",
         "review_body": "I loved my meal and the staff was super accomadating with substitutions. A super great menu selection too. Will definitely be getting the Veggie Scramble again.",
-    },
+    }
+    ]
 
 
