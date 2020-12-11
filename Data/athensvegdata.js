ceb693215352bcd0570d79a4c03013cbabb8a34e
Paris Johnson <51677972+pjj71370@users.noreply.github.com>
2020-12-11 06:48:28 -0500

    Update athensvegdata.js

--- Data/athensvegdata.js
+++ Data/athensvegdata.js
@@ -160,79 +160,198 @@
               "group_name": "Full",
               "menu_items": [
                 {
-                  "menu_item_name": "Dagwood",
-                  "menu_item_description": "Turkey, black forest ham, prime rib, shredded lettuce, tomatoes, onions, cheddar cheese, light mayo, horseradish dijon",
-                  "price": "9.50"
+                  "menu_item_name": "Mid-e Platter",
+                  "menu_item_description": "A Sampling Of Hummus, Falafel, And Tabouli Served With Lemon-tahini Dressing, Pita Points And Assorted Raw Vegetables.",
+                  "price": "$7.50"
                 },
                 {
-                  "menu_item_name": "Gyro",
-                  "menu_item_description": "Mediterranean seasoned beef, romaine, tomatoes, cucumbers, onions, black olives, feta cheese, tzatziki",
-                  "price": "9.50"
+                  "menu_item_name": "Hummus Side",
+                  "menu_item_description": "A Chick Pea, Tahini Dip Served With Pita Points.",
+                  "price": "$5.50"
                 },
                 {
-                  "menu_item_name": "Philly Steak",
-                  "menu_item_description": "Thinly sliced steak, grilled onions, grilled green peppers, grilled mushrooms, melted provolone cheese, ancho chipotle sauce",
-                  "price": "9.50"
+                  "menu_item_name": "Loaded Nachos",
+                  "menu_item_description": "A Generous Serving Of Corn Chips, Melted Cheese, Black Bean Chili, Lettuce, Red Onions, Sour Cream, And Our Homemade Salsa.",
+                  "price": "$7.25"
                 
                 },
                 {
-                  "menu_item_name": "Prime Rib",
-                  "menu_item_description": "Prime rib, shredded lettuce, tomatoes, onions, green peppers, cheddar cheese, horseradish dijon",
-                  "price": "9.50"
+                  "menu_item_name": "Nachos",
+                  "menu_item_description": "Corn Chips, Melted Cheese, And Our Own Salsa.",
+                  "price": "$4.25"
                  
                 },
                 {
-                  "menu_item_name": "Tuna",
-                  "menu_item_description": "Tuna, shredded lettuce, tomatoes, pickles, cheddar cheese, light mayo..",
-                  "price": "9.50"
+                  "menu_item_name": "Chips & Cheese Dip",
+                  "menu_item_description": "White Corn Chips With Premium Cheese Dipping Sauce.",
+                  "price": "$4.25"
                  
                 },
                 {
-                  "menu_item_name": "Turkey",
-                  "menu_item_description": "Deli style turkey, shredded lettuce, tomatoes, cucumbers, provolone cheese, light mayo",
-                  "price": "9.50"
+                  "menu_item_name": "Chips & Salsa",
+                  "menu_item_description": "White Corn Chips And Homemade Salsa.",
+                  "price": "$3.25"
                  
                 },
                 {
-                  "menu_item_name": "Club",
-                  "menu_item_description": "Black forest ham, turkey, bacon, shredded lettuce, tomatoes, provolone cheese, light honey dijon"
-                  
+                  "menu_item_name": "Split Pea Dal",
+                  "menu_item_description": "Indian Curry Flavor In A Deliciously Thick And Spicy Soup."
+                  "price": "$3.95"
                 },
                 {
-                  "menu_item_name": "Chicken Souvlaki",
-                  "menu_item_description": "Mediterranean seasoned chicken, spinach, tomatoes, cucumbers, onions, feta cheese, tzatziki"
-                
+                  "menu_item_name": "Soup Of The Day",
+                  "menu_item_description": "See Our Specials Board Or Ask About Today's Selection."
+                  "price": "$3.25"
                 },
                 {
                   "menu_item_name": "Chicken Crave®",
                   "menu_item_description": "Chicken breast, black forest ham, romaine, onions, provolone cheese, light honey dijon"
-                  
+                  "price": "$3.95"
                 },
                 {
-                  "menu_item_name": "Chicken Caesar",
-                  "menu_item_description": "Chicken breast, bacon, romaine, parmesan cheese, caesar dressing"
+                  "menu_item_name": "House Salad",
+                  "menu_item_description": "A Mountain Of Crisp Romaine And Tender Red Leaf Lettuces, Carrots, Purple Cabbage, Cucumbers, Tomatoes, And Red Onion Topped With Your Choice Of Sauteed Tofu Cubes Or Shredded White Cheddar."
+                  "price": "$7.25"
+                },
+                {
+                  "menu_item_name": "Tabouli Salad",
+                  "menu_item_description": "Middle Eastern Salad Featuring The Nutty Flavor Of Bulgur Tossed With Veggies And Spices. Served On A Bed Of Greens With Lemon-tahini Dressing."
+                  "price": "$2.95"
+                },
+                {
+                  "menu_item_name": "Petite Salad",
+                  "menu_item_description": "A Side Portion Of Lettuce, Purple Cabbage, Carrots, And Cucumbers."
+                  "price": "$2.95"
+                },
+                {
+                  "menu_item_name": "Rosa's",
+                  "menu_item_description": "Melted Cheese, Fresh Tomato Slices, Italian Spices, And Vinaigrette Dressing."
+                  "price": "$4.95"
+                },
+                {
+                  "menu_item_name": "Hummus",
+                  "menu_item_description": "Served With Cucumbers On A Toasted Bagel."
+                  "price": "$4.95"
+            },
+            {
+                  "menu_item_name": "Toasted",
+                  "menu_item_description": "With Butter Or Garlic Butter."
+                  "price": "$1.25"
+                },
+                {
+                  "menu_item_name": "Deluxe Grilled Steak",
+                  "menu_item_description": "Thinly-sliced, Marinated And Grilled Seitan Steak On A Toasted Hoagie With Sauteed Onions, Peppers, And Mushrooms. Topped With Melted White Cheddar, Freshly Cut Lettuce And Tomatoes, And Our Special Sauce. Vegan Without Cheese."
+                  "price": "$7.25"
+                },
+                {
+                  "menu_item_name": "Banh Mi",
+                  "menu_item_description": "Traditional Vietnamese Hoagie-style Sandwich With Hoisin-seared Seitan; Homemade Pickled Onions, Carrots, And Daikon; Jalapenos, Fresh Lettuce, And Diced Cucumber, With Wasabi-mayonnaise, And Topped With Fresh Cilantro. Vegan."
+                  "price": "$7.25"
+            },
+            {
+                  "menu_item_name": "The Seitan Gyro",
+                  "menu_item_description": "First The Greeks, And Now The Grit!! Zesty Greek-seasoned Seitan, Sizzled On The Grill With Roasted Red Peppers And Onion, Folded Into Toasted Pita Bread With Fresh Spinach And A Creamy Cucumber Tzitziki. Vegan.",
+                  "price": "$7.25"
+                },
+                {
+                  "menu_item_name": "The Grit B.o.d. (burger Of The Day)",
+                  "menu_item_description": "Delectible Grit Original Burger Creations, Changing From Day To Day. Your Server Will Have All The Scrumptious Details!",
+                  "price": "$7.25"
+                },
+                {
+                  "menu_item_name": "Teriyaki Tofu Wrap",
+                  "menu_item_description": "Tofu And Assorted Veggies Sauteed In Our Homemade Teriyaki Sauce And Wrapped In Pita.",
+                  "price": "$7.25"
                 
                 },
                 {
-                  "menu_item_name": "Chicken Breast",
-                  "menu_item_description": "Chicken breast, shredded lettuce, tomatoes, cheddar cheese, ranch dressing"
-                
+                  "menu_item_name": "Falafel",
+                  "menu_item_description": "Chick Pea Fritters In Pita Bread With Veggies And Lemon-tahini Dressing.",
+                  "price": "$6.75"
+                 
                 },
                 {
-                  "menu_item_name": "Buffalo Chicken",
-                  "menu_item_description": "Chicken breast grilled in buffalo sauce, romaine, tomatoes, onions, green peppers, provolone cheese, ranch"
-                  
+                  "menu_item_name": "Hummus",
+                  "menu_item_description": "Our Homemade Chick Pea Spread And Veggies Wrapped In Pita Bread With Lemon-tahini Or Vinaigrette Dressing.",
+                  "price": "$6.75"
+                 
                 },
                 {
-                  "menu_item_name": "B.L.T",
-                  "menu_item_description": "Bacon, shredded lettuce, tomatoes, light mayo"
-                
+                  "menu_item_name": "Grilled Cheese Smelt",
+                  "menu_item_description": "Your Choice Of Fresh Spinach Or Sliced Tomato Melted With Cheddar Cheese And Honey Mustard Dressing On Our Homemade Whole Wheat Bread. Athens Biggest And Best Grilled Cheese Sandwich.",
+                  "price": "$5.95"
+                 
                 },
                 {
-                  "menu_item_name": "Black Forest Ham",
-                  "menu_item_description": "Black forest ham, shredded lettuce, tomatoes, provolone cheese, yellow mustard, light mayo"
-                
-            }
+                  "menu_item_name": "Tofu Reuben Sandwich",
+                  "menu_item_description": "Round Marbled Rye Bread Grilled And Filled With Specially Seasoned Tofu, Swiss Cheese, Sauerkraut And Russian Dressing."
+                  "price": "$6.95"
+                },
+                {
+                  "menu_item_name": "Golden Bowl",
+                  "menu_item_description": "Browned Tofu Cubes Sauteed With Soy Sauce And Nutritional Yeast Served Over Brown Rice."
+                  "price": "$3.25"
+                },
+                {
+                  "menu_item_name": "The Grit Staple",
+                  "menu_item_description": "A Complete Protein Of Pinto Beans, Brown Rice, Melted Cheese, And Onions."
+                  "price": "$5.50"
+                },
+                {
+                  "menu_item_name": "Veggie Chili Cheese Dog",
+                  "menu_item_description": "A Hearty Veggie Frank On A Delicious Poppy Seed Bun With Our Famous Black Bean Chili, White Cheddar, And Side Of Chips."
+                  "price": "$7.25"
+                },
+                {
+                  "menu_item_name": "Black Bean Chili",
+                  "menu_item_description": "Our Popular House Recipe With Black Beans, Bulgur, Veggies, And Assorted, Zesty Spices. Topped With A Dollop Of Sour Cream."
+                  "price": "$5.75"
+                },
+                {
+                  "menu_item_name": "Falafel Platter",
+                  "menu_item_description": "Five Chickpea Fritters Served With Lemon-tahini Dressing, Pita Points Cucumbers, Carrots, Celery And Radishes."
+                  "price": "$7.50"
+                },
+                {
+                  "menu_item_name": "Tabouli Salad",
+                  "menu_item_description": "Middle Eastern Salad Featuring The Nutty Flavor Of Bulgur Tossed With Veggies And Spices. Served On A Bed Of Greens With Lemon-tahini Dressing."
+                  "price": "$4.95"
+                },
+                {
+                  "menu_item_name": "Hot Noodle Bowl Of The Day",
+                  "menu_item_description": "A Revolving Roster Of Asian-inspired Broths And Sauces In A Huge Steaming Bowl Of Noodles With A Variety Of Delicate Vegetable And Hearty Tofu Or Tempeh. See Board For Selection."
+                  "price": "$7.25"
+            },
+            {
+                  "menu_item_name": "Stir Fry",
+                  "menu_item_description": "Assorted Vegetable Sauteed In Our Own Teriyaki Sauce Served Over Brown Rice Or Noodles."
+                  "price": "$6.25"
+                },
+                {
+                  "menu_item_name": "Mondo Burrito",
+                  "menu_item_description": "A Large, Open-faced Burrito Layered With Brown Rice, Black Bean Chili, Veggies, Cheese And Salsa."
+                  "price": "$7.25"
+                },
+                {
+                  "menu_item_name": "Deluxe Quesadillas",
+                  "menu_item_description": "A Large Flour Tortilla With Your Choice Of Fillings. Comes With Chips, Salsa And Sour Cream."
+                  "price": "$7.25"
+            },
+            {
+                  "menu_item_name": "Pasta Marinara",
+                  "menu_item_description": "Linguine Pasta Smothered With Our Homemade Red Sauce. Comes With Garlic Toast."
+                  "price": "$6.50"
+            },
+            {
+                  "menu_item_name": "Stir Fry",
+                  "menu_item_description": "Assorted Vegetable Sauteed In Our Own Teriyaki Sauce Served Over Brown Rice Or Noodles."
+                  "price": "$6.25"
+                },
+                {
+                  "menu_item_name": "Pita Pizza,
+                  "menu_item_description": "Toasted Pita Bread With Our Marinara Sauce, Cheese, And Spices."
+                  "price": "$4.25"
+                }
             ]
             },
             {
